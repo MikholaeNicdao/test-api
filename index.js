324c1eb5f876b8a941be59aa7ebe4747696a2d1f
Mikholae Baniaga <mikholae.baniaga@gmail.com>
2022-06-18 19:50:28 +0800

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get('/', (req,res)=>{
-    res.json({success: "Succesasdasds"})
+    res.send("Hello World!")
 })
 
 app.listen(PORT, ()=>{
